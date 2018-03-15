6aa0383836d64a2590364aeb2167de89ccc294ad
Duminica Octavian <duminicaoctavian@duminicas-macbook-pro.local>
2018-03-15 04:50:41 +0200

    Initial Commit

---
+++ TrafficLights/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrafficLights
+//
+//  Created by Duminica Octavian on 15/03/2018.
+//  Copyright © 2018 Duminica Octavian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TrafficLights/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TrafficLights
+//
+//  Created by Duminica Octavian on 15/03/2018.
+//  Copyright © 2018 Duminica Octavian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
